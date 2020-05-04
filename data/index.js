a66d85daaa17fab2664bd65be5b3439ffb919318
WangHao <54601199+frank-wh@users.noreply.github.com>
2020-05-03 21:44:19 -0400

    Create index.js
    DATA file index.js

---
+++ data/index.js
@@ -0,0 +1,9 @@
+const usersData = require("./users");
+const housesData = require("./houses");
+const commentsData = require("./comments");
+
+module.exports = {
+  users: usersData,
+  houses: housesData,
+  comments: commentsData
+};
