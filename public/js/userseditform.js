dd69fa1916032db0fd74f0fdb0fc1d746112f7c9
WangHao <54601199+frank-wh@users.noreply.github.com>
2020-05-13 12:13:39 -0400

    Add files via upload

--- public/js/userseditform.js
+++ public/js/userseditform.js
@@ -1,45 +1,3 @@
-// $('#nav-login').hide();
-// $('#nav-new').hide();
-
-// $('#usersEditForm').submit((event) => {
-//     let hasError = false;
-//     $('#pwError').hide();
-//     event.preventDefault();
-//     const re = /^\w+$/;
-
-//     if(!$('#pw').val() && !$('#mail').val() && !$('#phoneNum').val()) {
-//         hasError = true;
-//     }
-
-//     if($('#pw').val()){
-//         if($('#pw').val() !== $('#pw2').val()){
-//             hasError = true;
-//             $('#pwError').show();
-//             $('#pwError').html('Error: Please check that you\'ve entered and confirmed your password!');
-//         }else if(!re.test($('#pw').val())) {
-//             hasError = true;
-//             $('#pwError').show();
-//             $('#pwError').html('Error: Password must contain only letters, numbers and underscores!');
-//         }else if($('#pw').val().length < 6) {
-//             hasError = true;
-//             $('#pwError').show();
-//             $('#pwError').html('Error: Password must contain at least six characters!');
-//         }else {
-//             const pw = $('#pw').val().toLowerCase();
-//             if($('#pw').val() === pw){
-//                 hasError = true;
-//                 $('#pwError').show();
-//                 $('#pwError').html('Error: Password must contain at least one uppercase letter (A-Z)!');
-//             }
-//         }
-//     }
-
-//     if(!hasError){
-//         $('#usersEditForm').submit();
-//     }
-// })
-
-
 let userEditForm = document.getElementById("usersEditForm");
 let password = document.getElementById("pw");
 let password2 = document.getElementById("pw2");
@@ -71,6 +29,11 @@ if(userEditForm){
                 hasError = true;
                 passwordError.hidden = false;
                 passwordError.innerHTML = 'Error: Password must contain at least six characters!';
+            } 
+            else if(password.value.length > 16){
+                hasError = true;
+                passwordError.hidden = false;
+                passwordError.innerHTML = 'Error: Password must contain at most sixteen characters!';
             } 
             else {
                 const pw = password.value.toLowerCase();
--- public/js/usersnewform.js
+++ public/js/usersnewform.js
@@ -1,70 +1,3 @@
-// $('#nav-logout').hide();
-// $('#nav-profile').hide();
-
-// $('#usersNewForm').submit((event) => {
-//     let hasError = false;
-
-//     $('#usernameError').hide();
-//     $('#passwordError').hide();
-//     $('#emailError').hide();
-//     $('#phoneNumError').hide();
-
-//     event.preventDefault();
-
-//     // check username
-//     if(!$('#username').val()) {
-//         hasError = true;
-//         $('#usernameError').show();
-//         $('#usernameError').html('Error: Please check that you\'ve entered an username');
-//     } else if($('#username').val().length < 6) {
-//         hasError = true;
-//         $('#usernameError').show();
-//         $('#usernameError').html('Error: Username must contain at least six characters!');
-//     }
-
-//     // check password
-//     const re = /^\w+$/;
-//     if(!$('#password').val() || !$('#password2').val() || $('#password').val() !== $('#password2').val()) {
-//         hasError = true;
-//         $('#passwordError').show();
-//         $('#passwordError').html('Error: Please check that you\'ve entered and confirmed your password!');
-//     } else if(!re.test($('#password').val())){
-//         hasError = true;
-//         $('#passwordError').show();
-//         $('#passwordError').html('Error: Password must contain only letters, numbers and underscores!');
-//     } else if($('#password').val().length < 6){
-//         hasError = true;
-//         $('#passwordError').show();
-//         $('#passwordError').html('Error: Password must contain at least six letters!');
-//     } else {
-//         const pw = $('#password').val().toLowerCase();
-//         if($('#password').val() === pw){
-//             hasError = true;
-//             $('#passwordError').show();
-//             $('#passwordError').html('Error: Password must contain at least one uppercase letter (A-Z)!');
-//         }
-//     }
-
-//     // check email
-//     if(!$('#email').val()){
-//         hasError = true;
-//         $('#emailError').show();
-//         $('#emailError').html('Error: Please check that you\'ve entered an email!');
-//     }
-
-//     // check phone number
-//     if(!$('#phoneNumber').val()) {
-//         hasError = true;
-//         $('#phoneNumError').show();
-//         $('#phoneNumError').html('Error: Please check that you\'ve entered a phone number!');
-//     }
-
-//     // submit without error
-//     if(!hasError){
-//         $('#userNewForm').submit();
-//     }
-// })
-
 let userNewForm = document.getElementById("usersNewForm");
 
 let username = document.getElementById("username");
@@ -103,6 +36,10 @@ if(userNewForm){
             hasError = true;
             usernameError.hidden = false;
             usernameError.innerHTML = 'Error: Username must contain at least six characters!';
+        } else if(username.value.length > 16){
+            hasError = true;
+            usernameError.hidden = false;
+            usernameError.innerHTML = 'Error: Username must contain at most sixteen characters!';
         }
         // check password
         const re = /^\w+$/;
@@ -118,6 +55,10 @@ if(userNewForm){
             hasError = true;
             passwordError.hidden = false;
             passwordError.innerHTML = 'Error: Password must contain at least six letters!';
+        } else if(password.value.length > 16){
+            hasError = true;
+            passwordError.hidden = false;
+            passwordError.innerHTML = 'Error: Password must contain at most sixteen letters!';
         } else {
             const pw = password.value.toLowerCase();
             if(password.value === pw){
